dac09195d4529ae93d26a897265c69547c54507d
Shuvo458 <155560696+Shuvo458@users.noreply.github.com>
2024-03-17 21:25:39 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// js
+//  windo alart
+function windowalart(){
+    window.alert(5 + 6);
+    // document.getElementById('')
+}
+
+// math
+function math(){
+    document.getElementById("dem").innerHTML = 5 + 6;
+}
+
+// head
+function myfunction(){
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+// show/hide
+function textshow(){
+    document.getElementById('text').style.display='block';
+}
+function texthide(){
+    document.getElementById('text').style.display='none';
+}
+
+
+//  color change 
+function changeColor(){
+    document.getElementById('c_c').style.color="blue";
+}
+
+// bulb on/off
+function bulbOn(){
+    document.getElementById('bulb').src ='imgas/on.gif';
+}
+function bulbOff(){
+    document.getElementById('bulb').src ='imgas/off.gif';
+}
+
+// show date 
+
+function ShowDate(){
+    document.getElementById('s_d').innerHTML = Date();
+}
+
+// text change
+function textChange(){
+    document.getElementById('t_c').innerHTML = "hello";
+}
